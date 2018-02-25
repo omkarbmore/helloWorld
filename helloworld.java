e95477dfa1bb5eafae43ce68a2eff53260d5a4cf
Omkar Balasaheb More <33678832+omkarbmore@users.noreply.github.com>
2018-02-25 21:18:02 +0530

    added only 3rd line

--- helloworld.java
+++ helloworld.java
@@ -2,7 +2,8 @@ public class HelloWorld
 {   
       public static void main(String[] args)
       {       
-         System.out.println("Hello gist @ GitHub!");  
-         System.out.println("punha syso"); 
+         
+            
+             System.out.println("teen patti");
       }
 } 
